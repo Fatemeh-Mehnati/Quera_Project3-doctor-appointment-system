48e66914537006b55c388bbb8b2a03e199c75891
ali soltani <alisoltani2210@gmail.com>
2026-09-04 21:40:13 +0330

    Implement custom user model and admin integration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,101 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+    )
+
+    search_fields = (
+        'email',
+        'first_name',
+        'last_name'
+    )
+    ordering = ('email',)
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'email', 'password',
+                )
+            }
+        ),
+        (
+            'personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'phone',
+                )
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
+        (
+            'Important dates',
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                    'created_at',
+                    'updated_at',
+
+                )
+            },
+        ),
+        (
+            'Creation information',
+            {
+                'fields': (
+                    'created_by_user',
+                )
+            },
+        ),
+    )
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+        'last_login',
+        'date_joined',
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'phone',
+                    'password1',
+                    'password2',
+                    'is_active',
+                    'is_staff',
+                ),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(max_length=255,unique=True)
+
+    phone = models.CharField(
+        max_length=11,
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    created_by_user = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='created_users',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
