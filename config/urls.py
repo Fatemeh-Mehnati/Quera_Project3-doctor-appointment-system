63be1ead01bf20b0f3f96541600ff97d97eff405
Fatemeh Mehnati <fatemeh.mehnati20@gmail.com>
2026-09-08 01:02:13 +0330

    Wire appointments URLs into project config

--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('doctors/', include('doctors.urls')),
+    path('appointments/', include('appointments.urls')),
+]
