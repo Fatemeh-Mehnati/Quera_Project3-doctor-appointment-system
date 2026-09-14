09ea7c9624c8a71a44dae9b90a63e37cfe94a179
Sam Ahmadi <samahmadi13851385@gmail.com>
2026-09-14 11:01:29 +0330

    7

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#z69=&b8uggx9)ebfxql%h=+5j&+j5y6bhnwdle+gco1g!_5vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost" , "127.0.0.1"]
 
 
 # Application definition
